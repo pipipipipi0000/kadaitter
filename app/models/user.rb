fd216f4add891d46270121b495f181e14fd47aa4
hirano <yprrrrr.mmmmkp@gmail.com>
2014-08-28 19:09:59 +0900

    Delete user's tweets when user is deleted

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :tweets
+  has_many :tweets, dependent: :destroy
   has_many :following_to, class_name: "Following", foreign_key: "from_id", dependent: :destroy
   has_many :followings, through: :following_to, source: :to
   has_many :followed_from, class_name: "Following", foreign_key: "to_id", dependent: :destroy
