a27e9f3879e02640890e50c8c6b84de335ef180e
hirano <yprrrrr.mmmmkp@gmail.com>
2014-08-29 18:01:37 +0900

    Require to sign in at users#show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action: set_user, only: %i(show)
+  before_action :authenticate_user!
+  before_action :set_user, only: %i(show)
 
   def show
     @user = User.find(params[:id])
