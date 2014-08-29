0fb30c1a4de02c2b4210d7c64143e5af1e6e543c
hirano <yprrrrr.mmmmkp@gmail.com>
2014-08-29 17:59:55 +0900

    Refactoring

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,15 @@
 class UsersController < ApplicationController
+  before_action: set_user, only: %i(show)
+
   def show
     @user = User.find(params[:id])
     @user_tweets = Tweet.includes(:user).where(user_id: @user.id).page(params[:page])
     @following = Following.find_or_initialize_by(from_id: current_user, to_id: @user)
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
